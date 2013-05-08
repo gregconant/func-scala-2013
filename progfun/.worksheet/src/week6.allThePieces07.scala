8a1111ab20d2f1e3fc20be5848017837ae760aa2
Greg Conant <greg.conant@gmail.com>
2013-05-08 07:37:16 -0400

    gc - starting worksheet for week 6, ep 7 (phone # mnemonics)

--- progfun/.worksheet/src/week6.allThePieces07.scala
+++ progfun/.worksheet/src/week6.allThePieces07.scala
@@ -1,11 +1,27 @@
 package week6
 
 object allThePieces07 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(82); 
-  println("Welcome to the Scala worksheet")}
+  println("Welcome to the Scala worksheet");$skip(236); 
 /*
 --------------------------
+Design a program that converts telephone #s to sentences.
 */
+  val mnemonics = Map(
+    '2' -> "ABC", '3' -> "DEF", '4' -> "GHI", '5' -> "JKL",
+    '6' -> "MNO", '7' -> "PRQS", '8' -> "TUV", '9' -> "WXYZ");System.out.println("""mnemonics  : scala.collection.immutable.Map[Char,String] = """ + $show(mnemonics ))}
+
 /*
+  Assume you are given a dictionary 'words' as a list of words.
+  
+  Design a method 'translate' such that
+    translate(phoneNumber)
+  produces all phrases of words that can serve as mnemonics for the
+  phone number.
+  
+  Ex: The phone number "7225247386"
+  should have the mnemonic "Scala is fun" as one element of the
+  set of solution phrases.
+
 --------------------------
 
 */
--- progfun/src/week6/allThePieces07.sc
+++ progfun/src/week6/allThePieces07.sc
@@ -4,8 +4,26 @@ object allThePieces07 {
   println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
 /*
 --------------------------
+Design a program that converts telephone #s to sentences.
 */
+  val mnemonics = Map(
+    '2' -> "ABC", '3' -> "DEF", '4' -> "GHI", '5' -> "JKL",
+    '6' -> "MNO", '7' -> "PRQS", '8' -> "TUV", '9' -> "WXYZ")
+                                                  //> mnemonics  : scala.collection.immutable.Map[Char,String] = Map(8 -> TUV, 4 -
+                                                  //| > GHI, 9 -> WXYZ, 5 -> JKL, 6 -> MNO, 2 -> ABC, 7 -> PRQS, 3 -> DEF)
+
 /*
+  Assume you are given a dictionary 'words' as a list of words.
+  
+  Design a method 'translate' such that
+    translate(phoneNumber)
+  produces all phrases of words that can serve as mnemonics for the
+  phone number.
+  
+  Ex: The phone number "7225247386"
+  should have the mnemonic "Scala is fun" as one element of the
+  set of solution phrases.
+
 --------------------------
 
 */
